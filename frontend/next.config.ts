1faf62d2d9a44143691476d1d6b88d36923a81fa
Tashif Imran <apka-email@example.com>
2026-07-10 18:20:43 +0500

    Updated config for static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Hugging Face par images load karne ke liye zaroori hai
+  },
 };
 
 export default nextConfig;
