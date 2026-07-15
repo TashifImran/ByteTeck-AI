60cba9875d1c808c14a334e824f8435107d062eb
Tashif Imran <apka-email@example.com>
2026-07-15 20:48:52 +0500

    Testing deployment

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
+import './globals.css'
 import type { Metadata } from "next";
-import "../globals.css";
 
 export const metadata: Metadata = {
-  title: "ByteTeck AI",
+  title: "ByteTeck",
   description: "Byteteck Chat Application",
 };
 
